d58796fa2fb71e21f6ae1a1d0ba081d7656c1e4b
Kathryn Forrest <kathryncforrest@gmail.com>
2026-03-31 19:14:35 -0400

    Update query service

--- src/query_service.py
+++ src/query_service.py
@@ -1,27 +1,63 @@
 import sqlite3
 import os
 import csv_loader
+import schema_manager
+
 
 # Use cin from command line
 def get_cli_command():
     while True:
-        ask = input("What do you want to do: 'load CSV', 'run SQL query', or 'exit'?")
-        if ask.lower()) == "load csv":
-            filename = input("What is the filename?")
+        ask = input("What do you want to do: 'load CSV', 'run SQL query', or 'exit'? ")
+        if ask.lower() == "load csv":
+            filename = input("What is the filename? ")
             df = read_csv(filename)
-            print("CSV successfully loaded.")
+            print("CSV successfully loaded. ")
             return df
-        else if ask.lower() == "run sql query":
-            query = input("Enter your SQL query:")
+        elif ask.lower() == "run sql query":
+            query = input("Enter your SQL query: ")
+            path = input("Enter your db path: ")
             print(f"Running query: {query}")
-            result = run_sql_query(query)
-        else if ask.lower() == "exit":
-            print("Exiting...")
+            result = run_sql_query(query, path)
+            return result
+        elif ask.lower() == "exit":
+            print("Exiting... ")
             break
-        else
-            print("That is not a valid statement. Please enter 'load CSV', 'run SQL query', or 'exit'")
+        else:
+            print("That is not a valid statement. Please enter 'load CSV', 'run SQL query', or 'exit': ")
+
+# Run a SQL Query
+def run_sql_query(query_text, db_path):
+    try:
+        # Connect to the schema manager
+        conn = schema_manager.init(db_path)
+
+        # Then connect to SQL to run and return the result
+        cursor.execute(query_text)
+
+        # Check if query is a select
+        if query_text.strip().lower().startswith("select"):
+            results = cursor.fetchall()
+            print("The query was a success! Results: ")
+            for row in results:
+                print(row)
+        else:
+            print("Please enter a select query: ")
+
+    # Check for database errors
+    except sqlite3.OperationalError as err:
+        print("SQL error: ", err)
+
+    except sqlite3.Error as err:
+        print("Database error: ", err)
+
+    # Once the query is done, close the connection
+    finally:
+        conn.close()
+
+
+        
 
 
 
-def load_cs
+
 
