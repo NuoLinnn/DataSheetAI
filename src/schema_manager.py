ef2ba29f45ad136fc35e2707f9230453228edb69
NuoLinnn <144183166+NuoLinnn@users.noreply.github.com>
2026-03-30 22:47:48 -0400

    add create_table function

--- src/schema_manager.py
+++ src/schema_manager.py
@@ -12,6 +12,7 @@ def init(db_path: str) -> sqlite3.Connection:
 
 def create_table_init_script(table_name: str, column_names:list[str]) -> str:
     """Sql script to create a table with given column names. PK will be autoincrement id"""
+
     columns = ["id INTEGER PRIMARY KEY AUTOINCREMENT"]
     for col in column_names:
         columns.append(f"{col} TEXT")
@@ -23,6 +24,13 @@ def create_table_init_script(table_name: str, column_names:list[str]) -> str:
     return sql
     
 
-def create_table(conn: sqlite3.Connection, column_names: list[str]):
+def create_table(conn: sqlite3.Connection, table_name: str, column_names:list[str]):
+    """calls sqlite3 to create table with given table_name and column_names"""
 
+    sql_script = create_table_init_script(table_name, column_names)
+
+    with conn:
+        conn.executescript(sql_script)
     
+    print("Table {table_name} created successfully")
+
